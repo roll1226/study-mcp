e9a3fa87a6598f6083baf629f17fcf125284db42
roll1226 <tomo_yasu_4724@icloud.com>
2025-04-20 07:09:44 +0900

    カラーパレットをcolors.tsに追加し、色定義を整理

--- src/consts/colors.ts
+++ src/consts/colors.ts
@@ -4,6 +4,8 @@ export const COLORS = {
   BUTTON: "#3DA9FC",
   BUTTON_TEXT: "#FFFFFE",
   BACKGROUND: "#FFFFFE",
+  STROKE: "#094067",
+  ERROR: "#D32929",
 } as const;
 
 export type COLORS = (typeof COLORS)[keyof typeof COLORS];
