5ef07d7119a796242f4aefb5f0867f08cae2f31b
roll1226 <tomo_yasu_4724@icloud.com>
2025-04-24 08:52:14 +0900

    Signupコンポーネントを追加し、Loginコンポーネントを削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
